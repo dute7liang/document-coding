6fb6c40f5bba35c49bdd726aee51ca99f4f94c11
duteliang <383200134@qq.com>
2019-12-08 16:31:28 +0800

    init zip util project

---
+++ tools/src/main/java/com/document/zip/AntZipUtil.java
@@ -0,0 +1,10 @@
+package com.document.zip;
+
+/**
+ * 利用Apache Ant的jar包来解压缩zip工具类。
+ * 相对于java原生的技术，可以支持中文
+ * @author: zl
+ * @Date: 2019/12/8 16:29
+ */
+public class AntZipUtil {
+}
---
+++ tools/src/main/java/com/document/zip/ZipUtil.java
@@ -0,0 +1,10 @@
+package com.document.zip;
+
+/**
+ * 利用java原生的类，来实现zip的解压缩工具类
+ * 需要注意的是java原生的不支持中文名
+ * @author: zl
+ * @Date: 2019/12/8 16:28
+ */
+public class ZipUtil {
+}
