49413af7a3d111d45dd2c95f99ea429090c1bc69
dute7liang <31098086+dute7liang@users.noreply.github.com>
2019-12-13 15:27:54 +0800

    处理代码！

--- web-base/src/main/java/com/duteliang/webbase/web/JspController.java
+++ web-base/src/main/java/com/duteliang/webbase/web/JspController.java
@@ -2,17 +2,22 @@ package com.duteliang.webbase.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author: zl
  * @Date: 2019/12/10 00:41
  */
 @Controller
+@RestController
 public class JspController {
 
 	@GetMapping("jsp")
 	public String jsp(){
 		return "hello";
 	}
+
+
+
 
 }
